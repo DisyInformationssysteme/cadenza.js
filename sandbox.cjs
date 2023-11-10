2a6cc5ba1fbed3b95760a465640b80b99dd81758
kissel <johannes.kissel@disy.net>
2023-11-10 13:08:32 +0100

    automatically confirm http-server installation

--- sandbox.cjs
+++ sandbox.cjs
@@ -4,4 +4,4 @@ const { URL } = require('url');
 const DEFAULT_CADENZA_URL = 'http://localhost:8080/cadenza';
 const cadenzaUrl = new URL((process.argv[2] === '--cadenza-url' && process.argv[3]) || process.env.CADENZA_URL || DEFAULT_CADENZA_URL);
 
-execSync(`npx http-server -a localhost -c-1 -d false --proxy ${cadenzaUrl.origin} --proxy-options.headers.Origin ${cadenzaUrl.origin} -o /sandbox.html${cadenzaUrl.pathname !== '/' ? `?contextPath=${cadenzaUrl.pathname}` : ''}`, { stdio: 'inherit' });
+execSync(`npx --yes http-server -a localhost -c-1 -d false --proxy ${cadenzaUrl.origin} --proxy-options.headers.Origin ${cadenzaUrl.origin} -o /sandbox.html${cadenzaUrl.pathname !== '/' ? `?contextPath=${cadenzaUrl.pathname}` : ''}`, { stdio: 'inherit' });
