3e05d125d1691a646f7bc4c8d196cbf9402f3390
bartels <andreas.bartels@disy.net>
2024-01-29 10:31:19 +0100

    404 reponse, if embedding target do not match the embeded target type

--- src/cadenza.js
+++ src/cadenza.js
@@ -300,6 +300,7 @@ export class CadenzaClient {
       operationMode,
       useMapSrs,
     });
+    params.append('targetType', 'MAP');
     await this.#show(resolvePath(mapView), params, signal);
     if (geometry) {
       this.#postEvent('setGeometry', { geometry });
