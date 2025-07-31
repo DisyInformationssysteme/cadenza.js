dd57ac96b0b53deedb8f384344804695271c55f8
Alex <40277484+qulol@users.noreply.github.com>
2025-07-31 19:51:17 +0200

    CADENZA-39576: Introduced UiFeature to disable dialog footers (#84) (#112)
    * CADENZA-39576: Introduced UiFeature to disable dialog footers (#84)
    
    * CADENZA-39576: Introduced UiFeature to disable dialog footers
    
    * CADENZA-39576: changed UIFeature name
    
    * CADENZA-39576: added ui feature for apply and cancel button
    
    * CADENZA-39574: enable post message communication for embedded content

--- src/cadenza.js
+++ src/cadenza.js
@@ -108,12 +108,15 @@ globalThis.cadenza = Object.assign(
 
 /** @typedef {'normal'|'simplified'} OperationMode - The mode in which a workbook should be operated */
 /**
- * @typedef {'workbook-design'|'workbook-map-add-layer'|'workbook-view-management'} UiFeature - The name of a Cadenza UI feature
+ * @typedef {'workbook-design'|'workbook-map-add-layer'|'workbook-view-management'|'embedded-dialog-footer-buttons'|'embedded-dialog-apply-button'|'embedded-dialog-cancel-button'} UiFeature - The name of a Cadenza UI feature
  *
  * _Note:_ Supported features are:
  * * `"workbook-design"` - The workbook designer
  * * `"workbook-map-add-layer"`- Add layers to the map
  * * `"workbook-view-management"` - Add/Edit/Remove workbook views (Is included in 'workbook-design'.)
+ * * `"embedded-dialog-footer-buttons"` - All footer buttons for Create/Edit Geometries and Select Objects dialogs
+ * * `"embedded-dialog-apply-button"` - Apply button for Create/Edit Geometries and Select Objects dialogs
+ * * `"embedded-dialog-cancel-button"` - Cancel button for Create/Edit Geometries and Select Objects dialogs
  * */
 
 /**
@@ -805,6 +808,7 @@ export class CadenzaClient {
    *
    * @param {EmbeddingTargetId} backgroundMapView - The workbook map view
    * @param {object} [__namedParameters] - Options
+   * @param {UiFeature[]} [__namedParameters.disabledUiFeatures] - Cadenza UI features to disable
    * @param {ExtentStrategy} [__namedParameters.extentStrategy] - Defines the initial map extent; If not given, Cadenza's default logic is used.
    * @param {FilterVariables} [__namedParameters.filter] - Filter variables
    * @param {(WorkbookLayerPath | string)[]} [__namedParameters.layers] - Layers to restrict the selection to
@@ -826,6 +830,7 @@ export class CadenzaClient {
   async selectObjects(
     backgroundMapView,
     {
+      disabledUiFeatures,
       extentStrategy,
       filter,
       layers,
@@ -845,6 +850,7 @@ export class CadenzaClient {
     });
     const params = createParams({
       action: 'selectObjects',
+      disabledUiFeatures,
       filter,
       layers: layers?.map(array),
       useMapSrs,
@@ -1370,6 +1376,9 @@ function validUiFeature(/** @type string */ value) {
     'workbook-design',
     'workbook-map-add-layer',
     'workbook-view-management',
+    'embedded-dialog-footer-buttons',
+    'embedded-dialog-apply-button',
+    'embedded-dialog-cancel-button',
   ].includes(value);
 }
 
