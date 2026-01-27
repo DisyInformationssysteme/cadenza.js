76132badb39b61586c11f4d353aecb4f591a0b62
schmidt <klaus.schmidt@disy.net>
2026-01-27 09:55:06 +0100

    CADENZA-39109 Add documentation of setCustomValidity now also applies to the selectObjects API call
    (cherry picked from commit 8394247d834fcbc764e818905268ee255673d851)

--- src/cadenza.js
+++ src/cadenza.js
@@ -985,11 +985,12 @@ export class CadenzaClient {
   }
 
   /**
-   * Set custom validity state of the geometry editor in addition to the default validation state (including errors and
-   * warnings). When set to error the dialog submission is blocked.
-   * If there already is a custom state set it will override it.
-   * Passing '' will reset the custom state to undefined, meaning no custom state is displayed.
-   * If no geometry editing is started, the method call has no effect.
+   * Set the custom validation status of the geometry editor dialog ( {@link createGeometry}, {@link editGeometry} )
+   * or the {@link selectObjects} dialog in addition to the default validation status (including errors and warnings).
+   * When set to 'error', the dialog submission is blocked.
+   * If a custom status is already set, it will be overwritten.
+   * Passing '' resets the custom status to undefined, which means no custom status is displayed.
+   * If neither the geometry editor nor {@link selectObjects} is started, the method call has no effect.
    * @param {string} message The message to show in the dialog
    * @param {CustomValidityType} [type] The type of message (defaults to 'error')
    */
